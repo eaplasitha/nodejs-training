ee537a60b4bdd762b613fd8bece5a834c234b678
lasitha edirisooriya <lasitha.edirisooriya@pearson.com>
2016-04-04 21:24:22 +0530

    first commit

---
+++ UnitTestingDemo-2/test/usersRouteTest.js
@@ -0,0 +1,51 @@
+var request = require('supertest');
+var express = require('express');
+var path = require('path');
+
+var routes = require('../routes/index');
+
+var app = express();
+
+app.use('/', routes);
+
+// view engine setup
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'jade');
+
+
+
+
+describe('UnitTestingDemo- Testing http://localhost:3000/ route when application running.', function () {
+
+  it('should return 200 on /', function (done) {
+
+    request('http://localhost:3000')
+      .get('/')
+      .expect(200)
+      .end(function(err, res){
+        if (err) return done(err)
+        done()
+      })
+
+  })
+
+})
+
+
+
+
+describe('UnitTestingDemo-  Testing http://localhost:3000/ route when application not running.', function () {
+
+  it('should return 200 on /', function (done) {
+
+    request(app)
+      .get('/')
+      .expect(200)
+      .end(function(err, res){
+        if (err) return done(err)
+        done()
+      })
+
+  })
+
+})
